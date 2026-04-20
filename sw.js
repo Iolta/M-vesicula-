4097ec77009bfed88eed6ff4177dca4ce5563b62
Iolta <profesorcarlosvidal@gmail.com>
2026-04-20 07:04:44 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vesicula-v1";
+const CACHE = "vesicula-v3";
 const ASSETS = ["./", "./index.html", "./app.js", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
